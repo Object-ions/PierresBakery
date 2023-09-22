34a4369bf68d6b7d42d5e49a50f3972bdfdfff4b
object-ions <moshikoatia@gmail.com>
2023-09-22 15:31:50 -0700

    add basic UI logic

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using PierresBakery.Models;
 
-class Program
+namespace PierresBakery
 {
-  static void Main()
+  class Program
   {
+    static void Main()
+    {
 
+      Console.WriteLine("Welcome to Pierre's Bakery");
+      Console.WriteLine("");
+      Console.WriteLine("We are offering a * special deal * over loaves of bread and pastries");
+      Console.WriteLine("a Loaf of bread is 5$ a pice OR buy 2 get 1 *free*");
+      Console.WriteLine("a Pasty is 2$ a pice OR buy 3 get 1 *free*");
+      Console.WriteLine("");
+
+      Console.WriteLine("How many loaves of bread would you like to get?");
+      string breadUserInput = Console.ReadLine();
+      int breadUserNumber = int.Parse(breadUserInput);
+
+      Console.WriteLine("How many pastries would you like to get?");
+      string pastryUserInput = Console.ReadLine();
+      int pastryUserNumber = int.Parse(pastryUserInput);
+
+      Console.WriteLine("");
+      Console.WriteLine($"The cost of {userInput} is : {result}");
+      Console.WriteLine("");
+
+
+      Console.WriteLine("done!");
+
+    }
   }
+
 }
