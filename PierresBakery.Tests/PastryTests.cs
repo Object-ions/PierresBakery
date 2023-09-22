dcc5f0743ebe1ca77ed5a6097c0f3e575201d693
object-ions <moshikoatia@gmail.com>
2023-09-22 10:04:00 -0700

    setting up files to work with TDD with MSTesting

---
+++ PierresBakery.Tests/PastryTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using PierresBakery.Models;
+using System;
+
+namespace PierresBakery.Tests
+{
+
+  [TestClass]
+  public class PastryTests
+  {
+    [TestMethod]
+    public void
+    {
+
+	// GetSide1_ReturnsSide1_Int()
+      
+      // Arrange
+      // Act
+      // Assert
+
+    }
+}
+}
