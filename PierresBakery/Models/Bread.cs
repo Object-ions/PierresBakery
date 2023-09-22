35528d7a71c7e4d1317226f5316d0683fc9e0d65
object-ions <moshikoatia@gmail.com>
2023-09-22 15:02:00 -0700

    update syntax

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -5,9 +5,10 @@ namespace PierresBakery.Models
   public class Bread
   {
     public static int breadPrice = 5;
+    public static int discountEvery = 3;
     public static int CalcCost(int userInput)
     {
-      int loavesToPay = userInput - (userInput / 3);
+      int loavesToPay = userInput - (userInput / discountEvery);
       int costOfBread = breadPrice * loavesToPay;
 
       return costOfBread;
