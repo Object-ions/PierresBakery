55f1c95e0783699dd4551cfe750e9bac049b51a0
object-ions <moshikoatia@gmail.com>
2023-09-22 16:20:39 -0700

    fix syntax error

--- PierresBakery/Models/Pastry.cs
+++ PierresBakery/Models/Pastry.cs
@@ -5,10 +5,10 @@ namespace PierresBakery.Models
   public class Pastry
   {
     public static int PastryPrice { get; set; } = 2;
-    public static int discountEvery { get; set; } = 4;
+    public static int DiscountEvery { get; set; } = 4;
     public static int CalcCost(int userInput)
     {
-      int pastriesToPay = userInput - (userInput / discountEvery);
+      int pastriesToPay = userInput - (userInput / DiscountEvery);
       int costOfPastry = PastryPrice * pastriesToPay;
 
       return costOfPastry;
