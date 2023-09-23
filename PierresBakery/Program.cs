3911f62ca123145522de849af924c962ca3f1704
object-ions <moshikoatia@gmail.com>
2023-09-22 17:34:53 -0700

    finilize UI

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -5,9 +5,9 @@ namespace PierresBakery
 {
   class Program
   {
+    static int totalCost = 0;
     static void Main()
     {
-
       Console.WriteLine("");
       Console.WriteLine("");
       Console.WriteLine("");
@@ -30,7 +30,7 @@ namespace PierresBakery
 
 
       PlaceOrder();
-      Console.WriteLine($"Now let's confirm your order");
+      Console.WriteLine($"Now let's confirm your order...");
       OrderStatus();
 
       Console.WriteLine("We appriciate your buisness");
@@ -38,57 +38,60 @@ namespace PierresBakery
       Console.WriteLine("Bon Apetit!");
       Console.WriteLine("");
 
+    }
+    public static void PlaceOrder()
+    {
+      Console.WriteLine("How many loaves of bread would you like to get?");
+      string breadUserInput = Console.ReadLine();
+      int breadUserNumber = int.Parse(breadUserInput);
+      int costOfBread = Bread.CalcCost(breadUserNumber);
 
-      public static void PlaceOrder()
+      Console.WriteLine("How many pastries would you like to get?");
+      string pastryUserInput = Console.ReadLine();
+      int pastryUserNumber = int.Parse(pastryUserInput);
+      int costOfPastry = Pastry.CalcCost(pastryUserNumber);
+
+      totalCost = costOfBread + costOfPastry;
+
+      Console.WriteLine("");
+      Console.WriteLine($"The cost of {breadUserNumber} loaves of bread is: ${costOfBread}");
+      Console.WriteLine($"The cost of {pastryUserNumber} pastries is: ${costOfPastry}");
+      Console.WriteLine($"Total cost is: ${totalCost}");
+      Console.WriteLine("");
+      Console.WriteLine("");
+    }
+
+    public static void OrderStatus()
+    {
+      Console.WriteLine("Please confirm the order by typing 'confirm'");
+      Console.WriteLine("or adjust the order by typing 'adjust'");
+      string confirmInput = Console.ReadLine();
+      if (confirmInput == "confirm")
       {
-        Console.WriteLine("How many loaves of bread would you like to get?");
-        string breadUserInput = Console.ReadLine();
-        int breadUserNumber = int.Parse(breadUserInput);
-        int costOfBread = Bread.CalcCost(breadUserNumber);
-
-        Console.WriteLine("How many pastries would you like to get?");
-        string pastryUserInput = Console.ReadLine();
-        int pastryUserNumber = int.Parse(pastryUserInput);
-        int costOfPastry = Pastry.CalcCost(pastryUserNumber);
-
-        int totalCost = costOfBread + costOfPastry;
-
-        Console.WriteLine($"The cost of {breadUserNumber} loaves of bread is: ${costOfBread}");
-        Console.WriteLine($"The cost of {pastryUserNumber} pastries is: ${costOfPastry}");
-        Console.WriteLine($"Total cost is: ${totalCost}");
         Console.WriteLine("");
-        Console.WriteLine("");
+        Console.WriteLine($"That's a total cost of: ${totalCost}");
+        Console.WriteLine($"Your order number is: {GenerateOrderNumber()}");
+        Console.WriteLine("Please check out with the cashier");
         Console.WriteLine("");
       }
-
-      public static void OrderStatus()
+      else if (confirmInput == "adjust")
       {
-        Console.WriteLine("Please confirm the order by typing 'confirm'");
-        Console.WriteLine("or adjust the order by typing 'adjust'");
-        string confirmInput = Console.ReadLine();
-        if (confirmInput == "confirm")
-        {
-          Console.WriteLine($"That's a total cost of: ${totalCost}");
-          Console.WriteLine($"Your order number is: {GenerateOrderNumber()}");
-          Console.WriteLine("Please check out with the cashier");
-        }
-        else if (confirmInput == "adjust")
-        {
-          Console.WriteLine("Let's make adjust your order!");
-          PlaceOrder();
-        }
-        else
-        {
-          Console.WriteLine("You typed and invalid answer, let's try again...");
-          OrderStatus()
-        }
+        Console.WriteLine("");
+        Console.WriteLine("Let's make adjustments to your order!");
+        PlaceOrder();
+        OrderStatus();
       }
-
-      public static int GenerateOrderNumber()
+      else
       {
-        return (int)(new Random().NextDouble() * 900000) + 100000;
+        Console.WriteLine("");
+        Console.WriteLine("You typed and invalid answer, let's try again...");
+        OrderStatus();
       }
     }
-  }
 
+    public static int GenerateOrderNumber()
+    {
+      return (int)(new Random().NextDouble() * 900000) + 100000;
+    }
+  }
 }
